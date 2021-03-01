79ca72f77f43f6071f16602c74292e86dc161e47
20mca1096 <70006954+20mca1096@users.noreply.github.com>
2021-03-01 11:38:34 +0530

    Add files via upload

---
+++ pract1.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Title of the document</title>
+    <style>
+      body {
+    background-image: url(b1.png);
+   
+    background-attachment: fixed;
+    
+}
+
+    </style>
+    <link rel="stylesheet" type="text/css" href="css1.css">
+    <script src='js1.js' crossorigin='anonymous'></script>
+  </head>
+  <body>
+    <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#Admissions">Admissions</a>
+  <a href="#news">News</a>
+  <a href="#about">About</a>
+  <a href="#jobs">Vacancies</a>
+  <a href="#contact">Contact</a>
+  <a href="#photos">Photo Gallery</a>
+ 
+</div>
+
+<div class="frame">
+      <img src="a4.jpg" alt="Nature">
+    </div>
+<div class="sidenav">
+  <div class="a1">LATEST NEWS</div>
+  <div class="a1">
+    <div style="width: 15%;float: left;">
+  <i style="font-size:18px;" class='fas'>&#xf15c;</i></div>
+  <div style="width: 85%;float: right;">Ihrs nur fruh und ward sie bei mund sirach solchh</div>
+</div>
+<div class="a1">
+    <div style="width: 15%;float: left;">
+  <i style="font-size:18px;" class='fas'>&#xf15c;</i></div>
+  <div style="width: 85%;float: right;">Ihrs nur fruh und ward sie bei mund sirach solchh</div>
+</div>
+<div class="a1">
+    <div style="width: 15%;float: left;">
+  <i style="font-size:18px;" class='fas'>&#xf15c;</i></div>
+  <div style="width: 85%;float: right;">Ihrs nur fruh und ward sie bei mund sirach solchh</div>
+</div>
+
+  <div class="a2">
+    <div class="content1"><a href="#news">ALL NEWS</a></div><div class="content2"><a href="#news">NEWS  </a></div>
+  </div>
+</div>
+<div class="sidenav2">
+  <div class="a1">Diary Dates</div>
+  <div class="a1">
+    <div style="width: 15%;float: left;">
+  <i style="font-size:18px;" class='fas'>&#xf073;</i></div>
+  <div style="width: 85%;float: right;">Ihrs nur fruh und ward sie bei mund sirach solchh</div>
+</div>
+<div class="a1">
+    <div style="width: 15%;float: left;">
+  <i style="font-size:18px;" class='fas'>&#xf073;</i></div>
+  <div style="width: 85%;float: right;">Ihrs nur fruh und ward sie bei mund sirach solchh</div>
+</div>
+<div class="a1">
+    <div style="width: 15%;float: left;">
+  <i style="font-size:18px;" class='fas'>&#xf073;</i></div>
+  <div style="width: 85%;float: right;">Ihrs nur fruh und ward sie bei mund sirach solchh</div>
+</div>
+  <div class="a2">
+    <div class="content1"><a href="#news">More Dates</a></div><div class="content2"><a href="#news">Calender</a></div>
+  </div>
+</div>
+<div class="new">
+  <h2 style="color: #320ad3">Welcome to Rising Mind Institutes</h2>
+ <div class="frame2">
+      <img src="a6.jpg" alt="Student">
+      <div class="p1">
+      <p>Verlassen mitwisser schwachen mannsbild ja du. Mi geworden wu ja lampchen zuweilen.</p>
+      <p>Gro neu warmer hierin stimme. Ubrigens sei gefallen allerlei dus sichtbar sie. Kammertur se ab hemdarmel einfacher.</p>
+      <p>Sah gewerbe vor wachsam familie unruhig gelegen regnete. Stockwerke eigentlich so. </p>
+    </div>
+    </div>
+</div>
+
+
+  </body>
+</html>
